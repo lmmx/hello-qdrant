affe5b07a4b9a9c50ba23b33a8c41244c570176e
Louis Maddox <louismmx@gmail.com>
2024-12-21 22:55:02 +0000

    update bench

--- README.md
+++ README.md
@@ -4,36 +4,47 @@ Setting up qdrant client for local use
 
 ## Timing benchmarks
 
-I include a simple benchmark of embedding 1,000 random `d_model` dimensional points for 6 cities,
-i.e. storing six thousand vectors in total.
+I include a simple benchmark of embedding random N-dimensional points for 6 cities
+(trivial modification of the demo example in their docs).
 
-Surprisingly, dimensionality of the vectors doesn't really matter: it performs poorly above 3 orders
-of magnitude regardless.
-
-The rate-limiting step is the upsert, poor performance beyond 1,000 points per city.
+- The rate-limiting step is the upsert.
+- Faster speeds are obtained over gRPC and using localhost rather than persisting to disk.
+    - In fact, persisting to disk is so rate limited that 1280D and 128D are the same speed.
+    - The speedup from using gRPC is roughly an order of magnitude.
 
 ### 128D
 
-- 35 seconds for 10^3
-- 3 seconds for 10^2
-- 0.3 seconds for 10^1
+- 98 seconds for 10^6
+- 7 seconds for 10^5
+- 0.9 seconds for 10^4
 
 ```
-Using 1,000 points in 128D space
-Build took 0.16 seconds
-Upsert took 35.61 seconds
+Using 10,020 points in 128D space in batches of 2,000
+Processing batch 1
+Processing batch 2
+Processing batch 3
+Processing batch 4
+Processing batch 5
+Processing batch 6
+Upsert took 0.97 seconds
 Query took 0.00 seconds
 ```
 
 ### 1280D
 
-- 40 seconds for 10^3
-- 3 seconds for 10^2
-- 0.3 seconds for 10^1
+- 496 seconds for 10^6
+- 33 seconds for 10^5
+- 3 seconds for 10^4
+- 0.3 seconds for 10^3
 
 ```
-Using 1,000 points in 1280D space                                                         
-Build took 0.38 seconds                                                                   
-Upsert took 38.89 seconds                                                                 
-Query took 0.02 seconds                                                                   
+Using 1,002,000 points in 1280D space in batches of 200000
+Processing batch 1
+Processing batch 2
+Processing batch 3
+Processing batch 4
+Processing batch 5
+Processing batch 6
+Upsert took 496.34 seconds
+Query took 0.50 seconds
 ```
--- src/embed.py
+++ src/embed.py
@@ -2,8 +2,7 @@ import time
 
 import numpy as np
 from qdrant_client import QdrantClient
-from qdrant_client.models import Distance, VectorParams
-from qdrant_client.models import PointStruct as PS
+from qdrant_client.models import Distance, PointStruct, VectorParams
 
 
 class Timer:
@@ -20,12 +19,16 @@ class Timer:
         print(f"{self.action} took {self.elapsed_time:.2f} seconds")
 
 
+cities = "Berlin London Moscow NYC Beijing Mumbai".split()
+n_cities = len(cities)
 d_model = 128
-n_points = 1_000
-print(f"Using {n_points:,} points in {d_model}D space")
+n_points_per = 16_700
+total_points = n_points_per * n_cities
+batch_size = 2_000
+print(f"Using {total_points:,} points in {d_model}D space in batches of {batch_size:,}")
 
 # client = QdrantClient(":memory:")  # in-memory Qdrant instance, for testing
-client = QdrantClient(path="./db")  # in-memory Qdrant instance, for testing
+client = QdrantClient(url="http://localhost:6333", prefer_grpc=True)
 if not client.collection_exists("test_collection"):
     client.create_collection(
         collection_name="test_collection",
@@ -38,20 +41,33 @@ def random_vector(seed: int):
     return rng.random(d_model).tolist()
 
 
-with Timer("Build"):
-    points = [
-        PS(
-            id=idx * n_points + frame_id,
-            vector=random_vector(seed=idx * n_points + frame_id),
-            payload={"city": city, "frame": frame_id},
-        )
-        for idx, city in enumerate("Berlin London Moscow NYC Beijing Mumbai".split())
-        for frame_id in range(n_points)
-    ]
+def yield_rows():
+    for frame_id in range(n_points_per):
+        for idx, city in enumerate(cities):
+            yield PointStruct(
+                id=idx * n_points_per + frame_id,
+                vector=random_vector(seed=idx * n_points_per + frame_id),
+                payload={"city": city, "frame": frame_id},
+            )
+
+
+batch_n = 0
+tbc = True
+generator = yield_rows()
 with Timer("Upsert"):
-    operation_info = client.upsert(
-        collection_name="test_collection", wait=True, points=points
-    )
+    while tbc:
+        batch_n += 1
+        print(f"Processing batch {batch_n}")
+        points = []
+        for n in range(batch_size):
+            try:
+                points.append(next(generator))
+            except StopIteration:
+                tbc = False
+        if points:
+            operation_info = client.upsert(
+                collection_name="test_collection", wait=True, points=points
+            )
 
 with Timer("Query"):
     search_result = client.query_points(
@@ -62,5 +78,5 @@ with Timer("Query"):
         limit=3,
     ).points
 
-for res in search_result:
-    print(res.model_dump_json(indent=2, exclude=["vector"], exclude_unset=True))
+# for res in search_result:
+#     print(res.model_dump_json(indent=2, exclude=["vector"], exclude_unset=True))
